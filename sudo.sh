147a2e97d8b17fca9c001ce6e7e060116afedeb6
GuilleGarrido <32095474+GuilleGarrido@users.noreply.github.com>
2019-05-18 10:44:15 +0200

    Delete sudo.sh

--- sudo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo emunIt19 | sudo -S docker start -a app
-echo emunIt19 | sudo -S docker cp app:/app/Brutos ./php-apache-mysql/app/ 
-echo emunIt19 | sudo -S docker exec mariadb /usr/bin/mysqldump --databases -u root --password=$PASSWORD predicciones > ./php-apache-mysql/predicciones.sql
